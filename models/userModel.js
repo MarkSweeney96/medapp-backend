123a9b122c874bca91dd1b6537b751f78dcef36c
MarkSweeney96 <marksweeney044@gmail.com>
2021-01-24 18:53:10 +0000

    changed min length to 5 for name on register

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, minlength: 7, unique: true },
   password: { type: String, required: true, minlength: 5 },
-  name: { type: String, required: true, minlength: 10 },
+  name: { type: String, required: true, minlength: 5 },
   address: { type: String, required: true, minlength: 10 },
   phone: { type: String, required: true, minlength: 5 },
 });
